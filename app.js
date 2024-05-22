b6880bd7ed88cb46e4c435341ce02dcacbf1b870
MateusMo <mat.marq.oliveira@gmail.com>
2024-05-22 15:11:03 -0300

    Acess improvements

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.engine('hbs', handlebars.engine({
     }
 }));
 
-
 app.use(express.static('public'));
 //Configuração bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -62,18 +61,6 @@ app.use(session({
 }));
 
 //roteador estático
-app.get('/', (req, res) => {
-    res.render('home')
-});
-app.get('/photos', (req, res) => {
-    res.render('photos', { times: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30] })
-});
-app.get('/grades', (req, res) => {
-    res.render('grades')
-});
-app.get('/contact', (req, res) => {
-    res.render('contact')
-});
 app.get('/login', (req, res) => {
     const user = req.session.user;
     if (user) {
@@ -82,29 +69,49 @@ app.get('/login', (req, res) => {
     res.render('login')
 });
 
-//Rotas restritas, tratar futuramente
-app.get('/userForm', (req, res) => {
+app.get('/', (req, res) => {
+    res.render('home')
+});
+app.get('/photos', (req, res) => {
+    res.render('photos', { times: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30] })
+});
+
+app.use('/publicGrade', publicGradeRouter);
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+});
+
+
+
+
+// Middleware de autenticação
+function authMiddleware(req, res, next) {
+    if (req.session.user) {
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+//Rotas restritas estáticas
+app.get('/userForm', authMiddleware,(req, res) => {
     res.render('adminContext/userForm')
 });
 
-app.get('/newGradeForm', (req, res) => {
+app.get('/newGradeForm',authMiddleware,(req, res) => {
     res.render('adminContext/newGradeForm')
 });
 
-app.get('/adminPanel', (req, res) => {
+app.get('/adminPanel',authMiddleware,(req, res) => {
     res.render('adminContext/adminPanel')
 });
-
-
-// Roteador dinâmico
-app.use('/access', loginRouter);
-app.use('/user', userRouter);
-app.use('/grade', gradeRouter);
-app.use('/publicGrade', publicGradeRouter);
-app.use('/contactAd', contactRouter);
-app.use('/logout', logoutRouter);
-
-
+// Roteador dinâmico com middleware de autenticação
+app.use('/access', authMiddleware, loginRouter);
+app.use('/user', authMiddleware, userRouter);
+app.use('/grade', authMiddleware, gradeRouter);
+app.use('/contactAd', authMiddleware, contactRouter);
+app.use('/logout', authMiddleware, logoutRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
